bad721356e1bb10ed5c1db65a346c093eb7ecce4
daniir <danrodas03@gmail.com>
2022-12-28 17:51:19 -0500

    React Components

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '../assets/vite.png'
+import routerLogo from '../assets/react-router-mark-color-inverted.svg'
+import reduxLogo from '../assets/redux.svg'
 
 export default function Footer() {
   return (
@@ -10,6 +12,10 @@ export default function Footer() {
           <img src={reactLogo} alt="reactLogo" height={50} width={50}/>
           <span className="text-white mx-2">+</span>
           <img src={viteLogo} alt="viteLogo" height={50} width={50} />
+          <span className="text-white mx-2">+</span>
+          <img src={routerLogo} alt="reactLogo" height={50} width={50}/>
+          <span className="text-white mx-2">+</span>
+          <img src={reduxLogo} alt="viteLogo" height={50} width={50} />
         </div>
         <div>
           <p className="text-white">Example app using <a className="link-warning" href="https://pokeapi.co/" target="_blank">PokeApi</a></p>
